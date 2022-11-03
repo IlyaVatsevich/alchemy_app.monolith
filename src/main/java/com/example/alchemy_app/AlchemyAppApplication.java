df027c007cf479141fab5d114f9e46b7c50b638c
Ilya Vatsevich <leone999@mail.ru>
2022-11-03 19:04:31 +0300

    add default

---
+++ src/main/java/com/example/alchemy_app/AlchemyAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.alchemy_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlchemyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlchemyAppApplication.class, args);
+    }
+
+}
